dea85c99509c86400105b68e2e0df6a59352d77e
janderrv26@gmail.com <janderrv26@gmail.com>
2022-10-28 18:43:18 -0400

    feat: add authors DTO

--- src/authors/dto/create-author.dto.ts
+++ src/authors/dto/create-author.dto.ts
@@ -1,1 +1,14 @@
-export class CreateAuthorDto {}
+import {IsString, IsNotEmpty, IsDate} from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateAuthorDto {
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+
+    @ApiProperty()
+    @IsDate()
+    @IsNotEmpty()
+    birthDate: Date;
+}
