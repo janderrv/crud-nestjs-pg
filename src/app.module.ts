be85a90699fbeed6c1812c72d5106a512237e9b7
janderrv26@gmail.com <janderrv26@gmail.com>
2022-10-28 19:10:55 -0400

    feat: add env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthorsModule } from './authors/authors.module';
+import { getEnvPath } from './common/helper/env.helper';
+
+const envFilePath: string = getEnvPath(`${__dirname}/common/envs`);
 
 @Module({
-  imports: [AuthorsModule],
+  imports: [ConfigModule.forRoot({ envFilePath, isGlobal: true })],
   controllers: [AppController],
   providers: [AppService],
 })
